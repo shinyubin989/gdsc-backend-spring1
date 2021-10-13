e4751f4356795b08d1608652aae675f34a25d416
shinyubin989 <dbqls2821@gmail.com>
2021-10-14 00:54:24 +0900

    assignment1-1

--- src/main/java/seoultech/gdsc/web/entity/User.java
+++ src/main/java/seoultech/gdsc/web/entity/User.java
@@ -28,10 +28,10 @@ public class User {
     @Column(nullable = false)
     private String email;
 
-    @Column(nullable = false)
+    @Column(nullable = false, length = 13)
     private String hp;
 
-    @Column(nullable = false)
+    @Column(nullable = false, length = 10)
     private String name;
 
     @Column(nullable = false)
@@ -44,12 +44,12 @@ public class User {
     private String profilePic;
 
 
-    @Column(nullable = false)
+    @Column(nullable = false, columnDefinition = "BOOLEAN DEFAULT FALSE")
     private Boolean isAuth = false;
 
-    @Column(nullable = false)
+    @Column(nullable = false, columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
     private LocalDateTime createdAt = LocalDateTime.now();
 
-    @Column(nullable = false)
+    @Column(nullable = false, columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP")
     private LocalDateTime updatedAt = LocalDateTime.now();
 }
